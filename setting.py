2d013ee4ee339c2bb9d935229577595fe6ed3a7f
aoshiman <men.katame.nori.oome@gmail.com>
2017-02-11 10:57:38 +0900

    something change

--- setting.py
+++ setting.py
@@ -1,7 +1,7 @@
 ## -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals, print_function
-from ast import literal_eval
+import yaml
 import lamvery
 
 UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E238 Safari/601.1"
@@ -10,11 +10,13 @@ END_POINT = '/recipe/hot'
 BUCKET = 'recipe.aoshiman.org'
 
 try:
-    _oauth_conf = literal_eval(lamvery.secret.get('secret'))
-    CONSUMER_KEY = _oauth_conf['CONSUMER_KEY']
-    CONSUMER_SECRET = _oauth_conf['CONSUMER_SECRET']
-    ACCESS_TOKEN = _oauth_conf['ACCESS_TOKEN']
-    ACCESS_SECRET = _oauth_conf['ACCESS_SECRET']
+    with open(lamvery.secret.file('config.yml')) as f:
+        _oauth_conf = yaml.load(f)
 
 except Exception as e:
     print(e)
+
+CONSUMER_KEY = _oauth_conf['consumer_key']
+CONSUMER_SECRET = _oauth_conf['consumer_secret']
+ACCESS_TOKEN = _oauth_conf['access_token']
+ACCESS_SECRET = _oauth_conf['access_secret']
