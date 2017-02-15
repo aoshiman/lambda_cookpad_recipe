cfe7372e52b9b98627ec7ebfcec206003e0641d3
aoshiman <men.katame.nori.oome@gmail.com>
2017-02-15 22:25:54 +0900

    update setting.py

--- setting.py
+++ setting.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals, print_function
 import yaml
 import lamvery
 
-UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13E238 Safari/601.1"
 DOMAIN = 'http://cookpad.com'
 END_POINT = '/recipe/hot'
 BUCKET = 'recipe.aoshiman.org'
@@ -20,3 +19,4 @@ CONSUMER_KEY = _oauth_conf['consumer_key']
 CONSUMER_SECRET = _oauth_conf['consumer_secret']
 ACCESS_TOKEN = _oauth_conf['access_token']
 ACCESS_SECRET = _oauth_conf['access_secret']
+UA = _oauth_conf['ua']
